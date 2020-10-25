21a0cce7382ffdf02806a0511d940c7ab1a92dc6
Derek Haynes <derek.haynes@gmail.com>
2020-10-25 13:21:13 -0600

    Added button web component

--- src/App.js
+++ src/App.js
@@ -1,22 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
 
+// Web Component
+import './web_components/button.js';
+
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <my-button label="Web Component - Click Me"></my-button>
       </header>
     </div>
   );
